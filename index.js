ff0fd606165c15497fa8c262a13c8526491f70d8
github username <kelly.hesterly@gmail.com>
2022-04-07 15:49:14 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (namesArray, occasion) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown (positiveInteger) {
+    while (positiveInteger > 0) {
+        console.log(positiveInteger);
+        positiveInteger -= 1;
+    }
+    console.log(positiveInteger);
+}
